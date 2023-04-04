ded592de4604474aa61e9f0006f8b5e2bf34df82
AJIT SHARMA <ajit08082000@gmail.com>
2023-04-04 23:54:04 +0530

    done array problem

--- ES6/neogAssignment/ques5.js
+++ ES6/neogAssignment/ques5.js
@@ -1,9 +1,18 @@
 // 5. Write an ES6 function that takes two array of numbers of equal length and returns a new array with the sum of each corresponding element in the two arrays. Avoid using in-built methods.
 
-export const sumArrays = () => {
+const sumArrays = (arr1, arr2) => {
     // Your ES6 code here
-
+    let newArr = [];
+    let sum;
+    for (let i = 0; i < arr1.length; i++) {
+        for (let j = 0; j <= i; j++) {
+            sum = (arr1[i] + arr2[j]);
+        }
+        newArr.push(sum);
+    }
+    return newArr;
 
 };
 
+console.log(sumArrays([1, 2, 3, 4], [1, 2, 3, 5])); // [2, 4, 6, 9]
 console.log(sumArrays([1, 2, 3, 4], [1, 2, 3, 5])); // [2, 4, 6, 9]
